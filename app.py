e26ce3e56f4b24957754bb7f27d55d348fc7fb3c
chancel <ycs1026@vip.qq.com>
2019-10-17 18:57:27 +0800

    [fix] 修复脚本一些问题 [imp] 整体签到速度加快 [imp] 返回如无异常直接输出为签到界面

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, send_file
+import time
 import os
+import sys
+
 
 app = Flask(__name__)
 
-@app.route('/',methods=["GET"])
-def index():
-     process = os.popen('./check-in.sh.sh')
-     output = process.read()
-     process.close()
-     return output
 
-app.run(host='0.0.0.0',port=30000)
+@app.route('/', methods=["GET"])
+def index():
+    script_path = sys.path[0] + "/check-in.sh"
+    process = os.popen(script_path)
+    output = process.read()
+    output = output.replace("\\n", "</br>")
+    process.close()
+    file_name = sys.path[0] + "/" + str(int(time.time())) + ".png"
+    os.popen("adb shell screencap -p | sed 's/\r$//' > " + file_name)
+    time.sleep(2)
+    if os.path.exists(file_name) is True:
+        return send_file(file_name, mimetype='image/gif')
+    if os.path.exists(file_name) is False:
+        return output
+
+
+app.run(host='0.0.0.0', port=30000)
--- check-in.sh
+++ check-in.sh
@@ -7,8 +7,10 @@
 #       输出为 mCurrentFocus=Window{424168a8 u0 com.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity
 #       com.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity 即钉钉的包/主页
 ########################################################################################################################
-
-phone_adb_url="192.168.10.120:5555"
+phone_local="yes"
+phone_adb_ip="192.168.10.120"
+phone_adb_port="5555"
+phone_model="A0001"
 dingding_package="om.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity"
 
 <<COMMENT
@@ -27,63 +29,63 @@ wait_tip(){
     echo ""
 }
 
-# 连接手机
-adb kill-server
-adb connect $phone_adb_url
-wait_tip "连接手机" 5
+phone_operating(){
+    adb shell am force-stop com.alibaba.android.rimet
+    wait_tip "Stop dingtalk application" 2
 
-echo "手机adb连接列表"
-adb devices
-devices=`adb devices`
-if [[ $devices =~ $phone_adb_url ]]
-then
-    echo "连接成功！"
-else
-    echo "错误：没有连接到设备"
-    wait_tip "自动打卡失败，程序正在退出" 3
-    exit 1
+    adb shell input keyevent 26              
+    adb shell am start -n com.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity
+
+    wait_tip "Start dingtalk application" 9
+
+    # 点击打卡界面，进入打卡，滑动界面，再点击打卡
+    adb shell input tap 540 1850
+    wait_tip "Waiting dingtalk loading check-in view" 9
+    i=0
+    while(($i > 3))
+    do
+        adb shell input swipe 500 300 500 1600              # 循环多次保证滑到最顶
+        let i++
+    done
+    adb shell input swipe 500 1650 500 1000
+    adb shell input swipe 500 1650 500 1000
+    adb shell input tap 140 450
+    wait_tip "Waiting dingtalk loading check-in view" 9
+
+    echo -n "Start check-in"
+    adb shell input tap 540 1150
+    adb shell input tap 205 1150
+    wait_tip "Waiting check-in success" 5
+    adb shell input tap 850 1060
+
+    echo -n "Congratulations!the script execute finish!!" 1
+}
+
+if [[ $phone_local == "yes" ]];then
+    phone_operating
+    exit 0
 fi
 
-# 初始化状态
-adb shell am force-stop com.alibaba.android.rimet
-wait_tip "初始化状态" 5
+# Check phone in this network
+ping -c 1 -w 5 $phone_adb_ip
+if [[ $? != 0 ]];then
+echo "sorry,your phone seem not in this network"
+exit 1
+fi
+echo "your phone is connect,waiting check adb connect"
 
-adb shell input keyevent 26                                                                                                                                                             
-adb shell input swipe 500 700 500 50
-wait_tip "解锁手机" 5
-
-# 根据查找到的钉钉包名称启动钉钉
-adb shell am start -n com.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity
-wait_tip "启动钉钉" 5
-wait_tip "等待钉钉加载成功" 20
-
-# 点击打卡界面，进入打卡，滑动界面，再点击打卡
-adb shell input tap 540 1850
-wait_tip "加载应用界面" 20
+# Check adb connecting phone status
+adb connect $phone_adb_ip:$phone_adb_port
 i=0
-while(($i > 3))
-do
-    adb shell input swipe 500 300 500 1600              # 循环多次保证滑到最顶
-    let i++
-done
-adb shell input swipe 500 1650 500 1000
-adb shell input swipe 500 1650 500 1000
-wait_tip "定位打卡入口" 5
-adb shell input tap 140 450
-wait_tip "进入打卡界面" 20
-
-y=700
-punch=0
-echo -n "开始打卡"
-while(($punch < 7))
-do
-    adb shell input tap 550 $y
-    let y=y+100
-    let punch++
-    echo -n "."
-    sleep 1s
-done
-
-# 关闭钉钉
-adb shell am force-stop com.alibaba.android.rimet
-wait_tip "打卡结束，正在关闭钉钉，拜拜！" 3
+adb_connect_result=`adb shell getprop ro.product.model`
+if [[ $adb_connect_result =~ $phone_model ]];then
+    phone_operating
+    exit 0
+fi
+adb kill-server
+adb connect $phone_adb_ip:$phone_adb_port
+adb_connect_result=`adb shell getprop ro.product.model`
+if [[ $adb_connect_result =~ $phone_model ]];then
+    phone_operating
+    exit 0
+fi
